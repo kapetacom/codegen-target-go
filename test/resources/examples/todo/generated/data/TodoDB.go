a9478b9658c9fbc5b4d7020634826a0d333699d7
Soren Mathiasen <sorenm@mymessages.dk>
2024-02-13 15:52:47 +0100

    feat: adding support for MongoDB

--- test/resources/examples/todo/generated/data/TodoDB.go
+++ test/resources/examples/todo/generated/data/TodoDB.go
@@ -1,4 +1,22 @@
 // GENERATED SOURCE - DO NOT EDIT
 package data
 
-// MongoDB not supported
+import (
+	"context"
+	"github.com/kapetacom/sdk-go-config/providers"
+	"github.com/kapetacom/sdk-go-nosql-mongodb"
+)
+
+func NewTodo(config providers.ConfigProvider) (*mongo.MongoDB, func(), error) {
+	// Create a new MongoDB client
+	client, err := mongo.NewMongoDB(config, "todo")
+	if err != nil {
+		return nil, nil, err
+	}
+	closeFunc := func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			panic(err)
+		}
+	}
+	return client, closeFunc, nil
+}
--- test/resources/examples/todo/main.go
+++ test/resources/examples/todo/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/kapeta/todo/generated"
 	"github.com/kapeta/todo/generated/auth"
+	"github.com/kapeta/todo/generated/data"
 	kapeta "github.com/kapetacom/sdk-go-config"
 	"github.com/labstack/echo/v4"
+	"log"
 )
 
 func main() {
@@ -30,6 +33,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	dbTodo, closeTodo, err := data.NewTodo(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeTodo()
 
 	// Start the server and log if it fails
 	e.Logger.Debug("Starting server on port " + port)
